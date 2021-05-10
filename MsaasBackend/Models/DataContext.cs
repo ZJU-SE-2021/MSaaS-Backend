cbf469ae28cf9a0cdd66a30a6e3352a4044e4a8c
Howyoung <howyoung3@gmail.com>
2021-05-10 23:39:55 +0800

    Add EF Core

---
+++ MsaasBackend/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MsaasBackend.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+    }
+}
