fdfcb83626be8a006a77f5d5c45f3038d8c33a4f
Howyoung <howyoung3@gmail.com>
2021-05-18 22:34:28 +0800

    fixup! Add Appointment entity

--- MsaasBackend/Models/Appointment.cs
+++ MsaasBackend/Models/Appointment.cs
@@ -4,6 +4,8 @@ namespace MsaasBackend.Models
 {
     public class Appointment
     {
+        public int Id { get; set; }
+
         public int UserId { get; set; }
         public User User { get; set; }
 
