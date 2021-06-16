cf51c6a950f86fb52066cbbdd50f05a0de28f3af
Howyoung <howyoung3@gmail.com>
2021-06-16 22:52:51 +0800

    Add tests for HospitalsController

--- MsaasBackend.Tests/IntegrationTests/BackendFactory.cs
+++ MsaasBackend.Tests/IntegrationTests/BackendFactory.cs
@@ -28,6 +28,28 @@ namespace MsaasBackend.Tests.IntegrationTests
                 Role = "Admin"
             };
             db.Users.AddRange(user, admin);
+
+            var hospital1 = new Hospital()
+            {
+                Name = "hospital 1"
+            };
+
+            var hospital2 = new Hospital()
+            {
+                Name = "hospital 2"
+            };
+
+            var hospital3 = new Hospital()
+            {
+                Name = "hospital 3"
+            };
+
+            var hospital4 = new Hospital()
+            {
+                Name = "hospital 4"
+            };
+
+            db.Hospitals.AddRange(hospital1, hospital2, hospital3, hospital4);
             db.SaveChanges();
         }
 
---
+++ MsaasBackend.Tests/IntegrationTests/HospitalsControllerTests.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using MsaasBackend.Models;
+using MsaasBackend.Tests.Utils;
+using Xunit;
+
+namespace MsaasBackend.Tests.IntegrationTests
+{
+    [Collection("Database collection")]
+    public class HospitalsControllerTests : TestBase
+    {
+        public HospitalsControllerTests(BackendFactory<Startup> factory) : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task GetHospitals_ValidIdentity_Success()
+        {
+            var res = await GetAs("/hospitals", Admin);
+            res.EnsureSuccessStatusCode();
+            var hospitals = await res.Content.ReadFromJsonAsync<ICollection<HospitalDto>>();
+            Assert.NotNull(hospitals);
+            Assert.InRange(hospitals.Count, 1, 6);
+        }
+
+        [Fact]
+        public async Task GetHospitalById_ValidIdentity_Success()
+        {
+            var res = await GetAs("/hospitals/1", Admin);
+            res.EnsureSuccessStatusCode();
+            var hospital = await res.Content.ReadFromJsonAsync<HospitalDto>();
+            Assert.Equal("hospital 1", hospital?.Name);
+        }
+
+        [Fact]
+        public async Task CreateHospital_ValidIdentity_Success()
+        {
+            var form = new HospitalCreationForm()
+            {
+                Name = "new hospital",
+                Address = "new address"
+            };
+
+            var res = await PostJsonAs("/hospitals", form, Admin);
+            res.EnsureSuccessStatusCode();
+            var hospital = await res.Content.ReadFromJsonAsync<HospitalDto>();
+            AssertExtensions.ContainsDeeply(form, hospital);
+        }
+
+        [Fact]
+        public async Task DeleteHospital_ValidIdentity_Success()
+        {
+            var res = await DeleteAs("/hospitals/3", Admin);
+            res.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task UpdateHospital_ValidIdentity_Success()
+        {
+            var form = new HospitalCreationForm()
+            {
+                Name = "updated hospital",
+                Address = "updated address"
+            };
+
+            var res = await PutJsonAs("/hospitals/4", form, Admin);
+            res.EnsureSuccessStatusCode();
+            var hospital = await res.Content.ReadFromJsonAsync<HospitalDto>();
+            AssertExtensions.ContainsDeeply(form, hospital);
+        }
+    }
+}
