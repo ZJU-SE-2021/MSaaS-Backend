bc58d119a799e12651fbf4f7fc5dd399bae90652
Howyoung <howyoung3@gmail.com>
2021-06-16 22:53:13 +0800

    Fix typo in namespace

--- MsaasBackend/Controllers/AppointmentsController.cs
+++ MsaasBackend/Controllers/AppointmentsController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using Msaasbackend.Controllers;
 using MsaasBackend.Models;
 
 namespace MsaasBackend.Controllers
