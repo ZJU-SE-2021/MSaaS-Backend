a9977ff030957c4e7746e03496c6a7dd0fb95dd9
Howyoung <howyoung3@gmail.com>
2021-06-26 22:03:47 +0800

    Add summary API

---
+++ MsaasBackend.Tests/IntegrationTests/SummaryTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http.Json;
+using System.Threading.Tasks;
+using MsaasBackend.Models;
+using Xunit;
+
+namespace MsaasBackend.Tests.IntegrationTests
+{
+    [Collection("Database collection")]
+    public class SummaryControllerTests : TestBase
+    {
+        public SummaryControllerTests(BackendFactory<Startup> factory) : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task GetSummary_ValidIdentity_Success()
+        {
+            var res = await GetAs("/summary");
+            res.EnsureSuccessStatusCode();
+            var summary = await res.Content.ReadFromJsonAsync<SummaryDto>();
+            Assert.NotNull(summary);
+        }
+    }
+}
---
+++ MsaasBackend/Controllers/SummaryController.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MsaasBackend.Models;
+
+namespace MsaasBackend.Controllers
+{
+    [Authorize(AuthenticationSchemes = AuthenticationDefaults.AuthenticationScheme)]
+    [ApiController]
+    [Route("[controller]")]
+    public class SummaryController : Controller
+    {
+        private readonly ILogger<SummaryController> _logger;
+
+        public SummaryController(ILogger<SummaryController> logger, DataContext context) : base(context)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(SummaryDto), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetSummary()
+        {
+            var userId = GetCurrentUserId();
+            if (!userId.HasValue) return Unauthorized();
+            var appointments =
+                from a in _context.Appointments
+                    .Include(a => a.User)
+                    .Include(a => a.Physician)
+                    .ThenInclude(p=>p.User)
+                    .Include(a => a.Physician)
+                    .ThenInclude(p => p.Department)
+                    .ThenInclude(d => d.Hospital)
+                    .Include(a => a.MedicalRecord)
+                where a.UserId == userId &&
+                      a.Time > DateTime.Now
+                orderby a.Time
+                select a;
+            var appointment = await appointments.FirstOrDefaultAsync();
+            var records =
+                from m in _context.MedicalRecords
+                    .Include(m => m.Appointment)
+                where m.Appointment.UserId == userId
+                orderby m.Appointment.Time descending
+                select m;
+            var record = await records.FirstOrDefaultAsync();
+            return Ok(new SummaryDto()
+            {
+                RecentAppointment = appointment?.ToDto(),
+                RecentMedicalRecord = record?.ToDto()
+            });
+        }
+    }
+}
